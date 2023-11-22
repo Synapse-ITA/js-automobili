ba031d939fae157f1907b685710906327af629e0
Synapse-ITA <puggillovalerio@gmail.com>
2023-11-22 15:40:50 +0100

    update

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Crea un array composto da 10 automobili. Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, elettrica, ibrida, gpl).
+
+let automobili = [
+    { marca: 'Toyota', modello: 'Corolla', alimentazione: 'ibrida' },
+    { marca: 'Tesla', modello: 'Model 3', alimentazione: 'elettrica'},
+    { marca: 'Volkswagen', modello: 'Golf', alimentazione: 'diesel'},
+    { marca: 'Renault', modello: 'Clio', alimentazione: 'gpl'},
+    { marca: 'Hyundai', modello: 'Kona', alimentazione: 'elettrica'},
+    { marca: 'Fiat', modello: '500', alimentazione: 'benzina'},
+    { marca: 'Honda', modello: 'Civic', alimentazione: 'ibrida'},
+    { marca: 'Chevrolet', modello: 'Bolt', alimentazione: 'elettrica'},
+    { marca: 'Mercedes', modello: 'Classe - A', alimentazione: 'diesel'},
+]
+
+// Dividi le automobili in 3 array separati:
+
+let autoBenzina = []
+let autoDiesel = []
+let autoGeneric = []
+
+array.forEach(function(element){
+    console.log(element);
+});
+
+
+
+
+
+
+// Infine stampa separatamente i 3 array.
