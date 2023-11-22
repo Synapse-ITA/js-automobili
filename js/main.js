c3cb76469c8dce855552cddff034461e91a81241
Synapse-ITA <puggillovalerio@gmail.com>
2023-11-22 17:00:18 +0100

    project done

--- js/main.js
+++ js/main.js
@@ -22,8 +22,17 @@ let autoDiesel = [];
 let autoGeneric = [];
 
 automobili.forEach(function (automobile) {
+    if (automobile.alimentazione === 'benzina') {
+        autoBenzina.push(automobile)
+    } else if (automobile.alimentazione === 'diesel') {
+        autoDiesel.push(automobile);
+    } else {
+        autoGeneric.push(automobile);
+    }
     console.log(automobile);
 });
+
+/* VERSIONE FOR NORMALE
 
 for (let i = 0; i < automobili.length; i++) {
     let automobile = automobili[i];
@@ -35,9 +44,28 @@ for (let i = 0; i < automobili.length; i++) {
         autoGeneric.push(automobile);
     }
 }
+*/
 
-// Infine stampa separatamente i 3 array.
+// Infine stampa separatamente i 3 array
 
-console.log(autoBenzina);
-console.log(autoDiesel);
-console.log(autoGeneric);
+console.log('La lista delle auto a benzina è:', autoBenzina);
+console.log('La lista delle auto a diesel è:', autoDiesel);
+console.log('La lista delle auto generiche è:', autoGeneric);
+
+
+// Inserire in HTML le 3 liste (array)
+
+stampaLista(autoBenzina, 'list1')
+stampaLista(autoDiesel, 'list2')
+stampaLista(autoGeneric, 'list3')
+
+// FUNZIONE PER STAMPARE
+
+function stampaLista(array, idLista) {
+    let listaHtml = '';
+    for (let i = 0; i < array.length; i++) {
+        let auto = array[i];
+        listaHtml += `<li>${auto.marca} ${auto.modello} ${auto.alimentazione}</li>`;
+    }
+    document.getElementById(idLista).innerHTML = listaHtml;
+}
